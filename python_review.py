4d0322e650ccde046ad32b80d0d1cc514987978c
mahshad golzarian <mahshad@mahshads-MacBook-Pro.local>
2025-04-08 19:05:44 -0400

    Level 4 complete

--- python_review.py
+++ python_review.py
@@ -1,3 +1,13 @@
 #Name : Mahshad Golzarian
 print("It worked!")
 X= None 
+example = "I'm a string" #The variable called example is no tholding the string value "i"m a string
+a = 3 # The variable called a is now holding the interger value 3
+b = 4.0 #The variable called b is now holding the float value 4.0
+c = True #The variable called c is now holding the boolean value True
+d = False #The variable called c is now holding the boolean value False
+e = "HEy!" #The variable called e is now holding the string value "HEY!"
+f = None #The variable called f is now holding the value None
+age = 32 #The variable called age is now holding the interger value 32
+name = "Insert your name here" # The variable called name is now holding the string "inster your name here"
+instrument = "insert your instrument here" #The variable instrument is now holding the string value "Insert your instrument here"
