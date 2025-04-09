cfe9531dfdc559d0f2f19051c335ab00b54111ab
mahshad golzarian <mahshad@mahshads-MacBook-Pro.local>
2025-04-08 20:25:02 -0400

    Level 9 problem 2 complete

---
+++ level9problem2.py
@@ -0,0 +1,11 @@
+x= input( "choose a number ")
+y= input("choose another one")
+firstNumber=int(x)
+secondNumber=int(y)
+if firstNumber > secondNumber :
+    print("The first number is greater than the second numebr")
+elif firstNumber < secondNumber :
+    print("The second number is greater than the first number")
+else :
+    print("the numbers are equal")
+
